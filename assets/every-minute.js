cc57761f4f08912fb380353784e7dce4e6383f59
Nathan Adam <nathaniellukasadam@gmail.com>
2019-03-10 21:48:24 -0700

    might work better without jquery

--- assets/every-minute.js
+++ assets/every-minute.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
    // playing this can't be done with the jquery function
-   var audioBox = document.getElementById("audioBox");
    var d;
    var played;
-   audioBox.load();
+   document.getElementById("audioBox").load();
 
    // this will run fifty times a second (for accuracy)
    setInterval(function() {
@@ -20,7 +19,7 @@ $(document).ready(function() {
       // d.getDay() == 6 && (d.getHours() == 9 || d.getHours() == 21) && d.getMinutes() == 0 && d.getSeconds() == 29 && !played
       if(d.getSeconds() == 29 && !played) {
          played = true;
-         audioBox.play();
+         document.getElementById("audioBox").play();
       }
    }, 20);
 });
