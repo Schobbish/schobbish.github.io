691314adb2cc40f4e53f929aae6d22ca53bb00bf
Nathan Adam <nathaniellukasadam@gmail.com>
2019-03-10 22:17:19 -0700

    stupid merge and made an incentive to make the user interact with page

--- assets/app.js
+++ assets/app.js
@@ -1,25 +1,28 @@
 $(document).ready(function() {
    // playing this can't be done with the jquery function
+   var audioBox = document.getElementById("audioBox");
    var d;
    var played;
-   document.getElementById("audioBox").load();
 
-   // this will run fifty times a second (for accuracy)
-   setInterval(function() {
-      d = new Date();
-      $('.clock').html(d.toLocaleTimeString('en-us'));
+   $('.clock').click(function() {
+       audioBox.load();
+       // this will run fifty times a second (for accuracy)
+       setInterval(function() {
+          d = new Date();
+          $('.clock').html(d.toLocaleTimeString('en-us'));
 
-      if(d.getDay() == 6) {
-         $('.day').html('Today is Saturday');
-      } else {
-         $('.day').html('Today is Not Saturday');
-      }
-      // if saturday and exactly at 08:59:29 (am or pm) and not played already
-      // correct if statement:
-      // d.getDay() == 6 && (d.getHours() == 8 || d.getHours() == 20) && d.getMinutes() == 59 && d.getSeconds() == 29 && !played
-      if(d.getDay() == 6 && (d.getHours() == 8 || d.getHours() == 20) && d.getMinutes() == 59 && d.getSeconds() == 29 && !played) {
-         played = true;
-         document.getElementById("audioBox").play();
-      }
-   }, 20);
+          if(d.getDay() == 6) {
+             $('.day').html('Today is Saturday');
+          } else {
+             $('.day').html('Today is Not Saturday');
+          }
+          // if saturday and exactly at 08:59:29 (am or pm) and not played already
+          // correct if statement:
+          // d.getDay() == 6 && (d.getHours() == 8 || d.getHours() == 20) && d.getMinutes() == 59 && d.getSeconds() == 29 && !played
+          if(d.getDay() == 6 && (d.getHours() == 8 || d.getHours() == 20) && d.getMinutes() == 59 && d.getSeconds() == 29 && !played) {
+             played = true;
+             audioBox.play();
+          }
+       }, 20);
+   });
 });
--- assets/every-minute.js
+++ assets/every-minute.js
@@ -1,25 +1,30 @@
 $(document).ready(function() {
    // playing this can't be done with the jquery function
+   var audioBox = document.getElementById("audioBox");
    var d;
    var played;
-   document.getElementById("audioBox").load();
 
-   // this will run fifty times a second (for accuracy)
-   setInterval(function() {
-      d = new Date();
-      $('.clock').html(d.toLocaleTimeString('en-us'));
+   $('.clock').click(function() {
+       audioBox.load();
+       // this will run fifty times a second (for accuracy)
+       setInterval(function() {
+          d = new Date();
+          $('.clock').html(d.toLocaleTimeString('en-us'));
 
-      if(d.getDay() == 6) {
-         $('.day').html('Today is Saturday');
-      } else {
-         $('.day').html('Today is Not Saturday');
-      }
-      // if saturday and exactly at 8:59:29 (am or pm) and not played already
-      // correct if statement:
-      // d.getDay() == 6 && (d.getHours() == 9 || d.getHours() == 21) && d.getMinutes() == 0 && d.getSeconds() == 29 && !played
-      if(d.getSeconds() == 29 && !played) {
-         played = true;
-         document.getElementById("audioBox").play();
-      }
-   }, 20);
+          if(d.getDay() == 6) {
+             $('.day').html('Today is Saturday');
+          } else {
+             $('.day').html('Today is Not Saturday');
+          }
+          // if saturday and exactly at 8:59:29 (am or pm) and not played already
+          // correct if statement:
+          // d.getDay() == 6 && (d.getHours() == 9 || d.getHours() == 21) && d.getMinutes() == 0 && d.getSeconds() == 29 && !played
+          if(d.getSeconds() == 29 && !played) {
+             played = true;
+             audioBox.play();
+          }
+       }, 20);
+   });
+
+
 });
